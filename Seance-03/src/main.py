8db4ed2fa7c86b7c98ab9f8c1392dba73a55b7fe
NathanCorcellut <nathan.corcellut@gmail.com>
2025-11-09 03:12:26 +0100

    S3 Q10 succes

--- Seance-03/src/main.py
+++ Seance-03/src/main.py
@@ -121,3 +121,107 @@ print("etendues =", etendues)
 
 
 
+## Question 7
+distances_interquart = []
+distances_interdec   = []
+for nom_colonne in liste_titres_col_quanti:
+    new_col = table[nom_colonne]
+
+    quart_25 = new_col.quantile(0.25)
+    quart_75 = new_col.quantile(0.75)
+    deci_10  = new_col.quantile(0.1)
+    deci_90  = new_col.quantile(0.9)
+
+    distances_interquart.append(round(float(quart_75 - quart_25),2))
+    distances_interdec.append(round(float(deci_90 - deci_10), 2))
+
+
+#OPTIONNEL :
+print("distances_interquart =" ,distances_interquart)
+print("distances_interdec =", distances_interdec)
+
+
+## Question 8
+
+for nom_colonne in liste_titres_col_quanti:
+    plt.boxplot(table[nom_colonne])
+    
+    plt.title("Boite à moustache pour les valeurs de la colonne {}".format(nom_colonne)) #Titre principal
+    plt.xlabel(nom_colonne) #Titre de l'axe vertical (X)
+    plt.ylabel("Effectif")  #Titre de l'axe vertical (Y)
+
+    plt.grid(True)              #Ajout de la grille
+
+    #Sauvegarde du résultat 
+    plt.savefig("./output/img/boite_moustache_pour_{}.png".format(nom_colonne))
+    plt.close() #Fermeture 'propre' du graphe
+
+
+## Question 9 
+# C'est fait, on a bien le deuxième .csv dans le fichier data
+
+
+
+## Question 10
+
+with open("./data/island-index.csv", "r") as fichier :
+    contenu = pd.read_csv(fichier)
+
+iles = pd.DataFrame(contenu)
+
+surfaces = iles["Surface (km²)"]
+
+print(surfaces)
+
+# Oranigramme :
+# La surface est-elle inférieure à 10 ?
+#   |
+#   --OUI: On enregistre dans ]0,10]
+#   |
+#   --NON: La surface est-elle inférieure à 25 ?
+#       |
+#       --OUI: On enregistre dans ]10,25]
+#       |
+#       --NON: La surface est-elle inféreieure à 50 ?
+#           |
+#           --OUI: On enregistre dans ]25,50]
+#           |
+#           --NON: La surface est-elle inféreieure à 100 ?
+#               |
+#               etc...
+
+compte_0_10       = 0
+compte_10_25      = 0
+compte_25_50      = 0
+compte_50_100     = 0
+compte_100_2500   = 0
+compte_2500_5000  = 0
+compte_5000_10000 = 0
+compte_10000_INF  = 0
+
+for surface in surfaces:
+    if surface <= 10.0:
+        compte_0_10 += 1
+    elif surface <= 25.0:
+        compte_10_25 += 1
+    elif surface <= 50.0:
+        compte_25_50 += 1
+    elif surface <= 100.0:
+        compte_50_100 += 1
+    elif surface <= 2500.0:
+        compte_100_2500 += 1
+    elif surface <= 5000.0:
+        compte_2500_5000 += 1
+    elif surface <= 10000.0:
+        compte_5000_10000 += 1
+    else :
+        compte_10000_INF += 1
+
+print("Entre 0 et 10km², on dénombre {} îles.".format(compte_0_10))
+print("Entre 10 et 25km², on dénombre {} îles.".format(compte_0_10))
+print("Entre 25 et 50km², on dénombre {} îles.".format(compte_0_10))
+print("Entre 50 et 100km², on dénombre {} îles.".format(compte_0_10))
+print("Entre 100 et 2 500km², on dénombre {} îles.".format(compte_0_10))
+print("Entre 2 500 et 5 000km², on dénombre {} îles.".format(compte_0_10))
+print("Entre 5 000 et 10 000km², on dénombre {} îles.".format(compte_0_10))
+print("Au delà de 10 000km², on dénombre {} îles.".format(compte_0_10))
